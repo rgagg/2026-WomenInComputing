3269c4015c50e0ef6ed73fb743cd81acc877b1cc
Richard Gagg <r.gagg@outlook.com>
2026-02-03 17:20:39 +1100

    Initial Commit

---
+++ WomenInComputing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WomenInComputing
+//
+//  Created by Richard Gagg on 3/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by Richard Gagg on 3/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
